84244936140a164883a8236f488a53aef3df1f12
Igor Valeriu Railean <igor-valeriu.railean@intern.arobs.com>
2020-09-10 15:51:45 +0300

    null

--- fastcore/transform.py
+++ fastcore/transform.py
@@ -100,6 +100,7 @@ class Transform(metaclass=_TfmMeta):
         print("In Transform: setup: self.setups type ", type(self.setups), " type self ", type(self), " self.setups: ", self.setups)
         print("In Transform: type(items)", type(items), " call getattr(items, 'train', items)", getattr(items, 'train', items))
         print("CALL self.setups: " , self.setups(getattr(items, 'train', items) if train_setup else items))
+        print("CALL FINISHED")
         return self.setups(getattr(items, 'train', items) if train_setup else items)
 
     def _call(self, fn, x, split_idx=None, **kwargs):
