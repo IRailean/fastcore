767c8d31c48bdda9678f3840e7498532524936be
Igor Valeriu Railean <igor-valeriu.railean@intern.arobs.com>
2020-09-10 13:59:07 +0300

    null

--- fastcore/transform.py
+++ fastcore/transform.py
@@ -102,7 +102,6 @@ class Transform(metaclass=_TfmMeta):
             print("In Transform: setup: self setups is null")
         else:
             print("In Transform: setup: self.setups: ", self.setups)
-        print("In Transform: setup: help self setups ", help(self.setups), "help self ", help(self))
         return self.setups(getattr(items, 'train', items) if train_setup else items)
 
     def _call(self, fn, x, split_idx=None, **kwargs):
